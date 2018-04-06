a671cfeed3073a52ef6932a8f7fb3d491c4fe2d4
johir-rayhan <johir.rayhan01@gmail.com>
2018-04-06 11:56:14 +0600

    bad code in Header cmponent subtitle props is not define but it's h1 tag show in web dev tool

--- src/index.js
+++ src/index.js
@@ -61,7 +61,8 @@ class IndecisionApp extends Component{
 }
 
 const Header = (props) => {
-        return (<div><h1>{props.title}</h1></div>);
+        return (<div><h1>{props.title}</h1>
+            <h1>{props.subtitle}</h1></div>);
 }
 
 Header.defaultProps = {
