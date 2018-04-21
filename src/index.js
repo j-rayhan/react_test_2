a40a7539ceb9bf33c5f267a928e06678f2aa70e2
johir-rayhan <johir.rayhan01@gmail.com>
2018-04-21 16:02:34 +0600

    setting up webpack with SCSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './component/IndecisionApp';
 import { createClient } from 'http';
+import "./styles/styles.scss";
 
 const mountNode = document.getElementById('app');
 ReactDOM.render(<IndecisionApp />,mountNode);
