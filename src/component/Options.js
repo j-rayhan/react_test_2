a427b3c9b20741ab5db90f21a74eb4ac28b47c6a
johir-rayhan <johir.rayhan01@gmail.com>
2018-04-27 08:09:14 +0600

    068 Big Button  Options List

--- src/component/Options.js
+++ src/component/Options.js
@@ -3,7 +3,7 @@ import Option from './Option';
 
 const Options = (props) => (
     <div>
-        <button onClick={props.removeAllOptions}>Remove All </button>
+        <button className="button button--link" onClick={props.removeAllOptions}>Remove All </button>
         {props.options.length === 0 && <p> Please add an option to get started!</p>}
         {props.options.map((option) => (<Option key={option} optionTex={option} removeOption={props.removeOption} />))}
     </div>
