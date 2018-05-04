bcfa0a95f7e80d22b02098aa1bb0095d1bb80809
johir-rayhan <johir.rayhan01@gmail.com>
2018-05-04 17:22:59 +0600

    show props in console which are render by browserrouter

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is Dashboard
-    </div>
-);
+const ExpenseDashboardPage = (props) =>{
+
+    console.log(props);
+    // Also use this example http://localhost:8080/?query?=rent&sort=date "see in location key"
+    return (
+        <div>
+            This is Dashboard
+        </div>
+    );
+};
 
 
 export default ExpenseDashboardPage;
