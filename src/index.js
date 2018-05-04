4e2e1e3840c12ce47592ada40dd8baacab81af8c
johir-rayhan <johir.rayhan01@gmail.com>
2018-05-04 12:27:50 +0600

    Add header routing link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route , Switch} from 'react-router-dom';
+import { BrowserRouter, Route , Switch, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import "./styles/styles.scss";
 
@@ -16,18 +16,36 @@ const AddExpensePage = () =>(
     </div>
 )
 
+const Help = () =>(
+    <div>
+        This is Help Page
+    </div>
+)
 const NotFoundPage = () =>(
     <div>
-        404!
+        404! <Link to="/">Go Home</Link>
     </div>
+)
+
+const Header = () =>(
+    <header>
+        <h1>header </h1>
+        <Link to="/">Home </Link>
+        <Link to="/create"> Add </Link>
+        <Link to="/help"> Help </Link>
+    </header>
 )
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage} />
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/help" component={Help} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
 )
 const mountNode = document.getElementById('app');
