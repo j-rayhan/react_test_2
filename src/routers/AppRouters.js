2c90c33a000a57585e46cf04c03b6905501f8f63
johir-rayhan <johir.rayhan01@gmail.com>
2018-05-04 23:57:03 +0600

    081 Query Strings and URL Parameters

--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route , Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route , Switch } from 'react-router-dom';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
+import EditPage from '../components/EditPage';
 import Help from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
@@ -13,6 +14,7 @@ const AppRouters = () => (
             <Switch>
                 <Route path="/" component={ExpenseDashboardPage} exact={true}/>
                 <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit/:id" component={EditPage} />
                 <Route path="/help" component={Help} />
                 <Route component={NotFoundPage} />
             </Switch>
