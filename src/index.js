20750954d03678948950457b99952590117f7231
johir-rayhan <johir.rayhan01@gmail.com>
2018-05-04 12:10:27 +0600

    078 Setting up a 404

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter,Route} from 'react-router-dom';
+import { BrowserRouter, Route , Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import "./styles/styles.scss";
 
@@ -16,12 +16,18 @@ const AddExpensePage = () =>(
     </div>
 )
 
+const NotFoundPage = () =>(
+    <div>
+        404!
+    </div>
+)
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true}/>
             <Route path="/create" component={AddExpensePage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 )
 const mountNode = document.getElementById('app');
