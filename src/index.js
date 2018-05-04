b6b51366562e169517d7bebf7bd532d3e4a2064d
johir-rayhan <johir.rayhan01@gmail.com>
2018-05-04 12:04:08 +0600

    077 React-Router 101

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter,Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import "./styles/styles.scss";
 
+const ExpenseDashboardPage = () =>(
+    <div>
+        This is Dashboard
+    </div>
+)
+
+const AddExpensePage = () =>(
+    <div>
+        This is Another Component
+    </div>
+)
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage} />
+        </div>
+    </BrowserRouter>
+)
 const mountNode = document.getElementById('app');
-ReactDOM.render(<p>Hello</p>,mountNode);
+ReactDOM.render(routes,mountNode);
