e3a28030460d1a7a0f66f8be9948f328509a5281
johir-rayhan <johir.rayhan01@gmail.com>
2018-05-04 11:42:44 +0600

    Boilerplate V_1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import "./styles/styles.scss";
 
-import IndecisionApp from './component/IndecisionApp';
-
 const mountNode = document.getElementById('app');
-ReactDOM.render(<IndecisionApp />,mountNode);
+ReactDOM.render(<p>Hello</p>,mountNode);
