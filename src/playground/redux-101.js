c29c88b2a3693104f959878b1375af5802880268
johir-rayhan <johir.rayhan01@gmail.com>
2018-05-05 18:15:21 +0600

    087 Subscribing and Dynamic Actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,12 +4,18 @@ const store = createStore((state = {count: 10}, action) => {
     // console.log('running');
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 0 ;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 0 ;
         return {
-            count: state.count - 1
+            count: state.count - decrementBy
+        };
+        case 'SET':
+        return {
+            count: action.count
         };
         case 'RESET':
         return {
@@ -21,32 +27,36 @@ const store = createStore((state = {count: 10}, action) => {
     return state;
 });
 
-console.log(store.getState());
+// subscirbe for call every dispatch action
+const unSubscribe = store.subscribe(() =>{
+    console.log(store.getState());
+});
 
-// Do this Actions
-// I'd like to increment the count
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 5
+});
+
+// Un Subscribe for stop the subscribe fun
+// unSubscribe();
+
 store.dispatch({
     type: 'INCREMENT'
 });
 
-console.log(store.getState());
-
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'DECREMENT',
+    decrementBy: 2
 });
 
-console.log(store.getState());
-
-// I'd like to decrement the count
-store.dispatch({
-    type: 'DECREMENT'
-});
-
-console.log(store.getState());
-
-// I'd like to reset the count
 store.dispatch({
     type: 'RESET'
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
+
+
+
